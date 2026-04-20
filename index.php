529e0cf83ecf2ff14119927e953f8969e0944a6a
riseswatson6532-dotcom <riseswatson6532@gmail.com>
2026-04-20 21:53:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 <html lang="en">
 
 <head>
+  <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5021787,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5021787&101" alt="counter stats" border="0"></a></noscript>
+<!-- Histats.com  END  -->
+
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
